4f2be858fa25e29d87202ac83c2e15d678878e83
Harley Hanna <harleyhannawrit@gmail.com>
2020-05-31 15:20:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,8 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, player_character)
-  
+  if board[index] == " "
+    return false
+  elsif board[index] == "X"
+    return true
 end
