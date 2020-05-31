9b3bff3219091e2f52a5e35d989c2b07213443ce
Harley Hanna <harleyhannawrit@gmail.com>
2020-05-31 15:24:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif board[index] == "X"
-    return true
-  elsif board[index] == "O"
+  elsif board[index] == "X" || board[index] == "O"
     return true
   end
 end
